60a9e3539a46bb3dcc40a5ee265ae90da48afa2a
shivanidighe01 <shivanidighe2003@gmail.com>
2024-06-23 17:58:00 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Tomato helps you find and order food from wherever you are. How it works: you type in an address, we tell you the restaurants that deliver to that locale as well as showing you droves of pickup restaurants near you.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Navbar.css'
+import { useState } from 'react'
+const Navbar = () => {
+
+  const [menu, setMenu] = useState("Home")
+
+  return (
+   <>
+   <div className='navbar'>
+    <img className='logo' src={assets.logo} alt="logo" />
+   
+     <ul className="nav-menu">
+      <li onClick={()=>setMenu("Home")} className={menu==='Home'?"active":""}>Home</li>
+      <li onClick={()=>setMenu("Menu")} className={menu==='Menu'?"active":""}>Menu</li>
+      <li onClick={()=>setMenu("Mobile-App")} className={menu==='Mobile-App'?"active":""}>Mobile-App</li>
+      <li onClick={()=>setMenu("Contact Us")} className={menu==='Contact Us'?"active":""}>Contact Us</li>
+     </ul>
+    
+    <div className="nav-right">
+      <img src={assets.search_icon} alt="search" />
+      <div className="search-icon">
+        <img src={assets.basket_icon} alt="basket" />
+        <div className="dot"></div>
+      </div>
+      <button>Sign in</button>
+    </div>
+   
+   </div>
+   </>
+  )
+}
+
+export default Navbar
