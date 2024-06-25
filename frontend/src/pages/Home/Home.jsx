2a1b798e1146ea9c542b5f22868adcd80518bdfd
shivanidighe01 <shivanidighe2003@gmail.com>
2024-06-25 12:43:19 +0530

    added menu list

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 
 import './Home.css'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+
+
+
+
 const Home = () => {
+
+  const [category, setCategory] = useState("All")
   return (
     <>
       <Header></Header>
+      <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+      <FoodDisplay category={category}></FoodDisplay>
     </>
   )
 }
