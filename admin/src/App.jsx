0fc4c81d2f609b92fe85c26e607e8420752cb0a9
shivanidighe01 <shivanidighe2003@gmail.com>
2024-07-24 21:56:36 +0530

    admin folder setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-
+import {Routes, Route } from 'react-router-dom'
+// import List from './pages/List/List'
+// import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
 const App = () => {
   return (
     <div>
@@ -9,6 +14,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar></Sidebar>
+        <Routes>
+          <Route path='/add' element={<Add></Add>}></Route>
+          <Route path='/list' element={<List></List>}></Route>
+          <Route path='/orders' element={<Orders></Orders>}></Route>  
+        </Routes>
       </div>
     </div>
   )
---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Add.css'
+const Add = () => {
+  return (
+    <div>
+        add
+    </div>
+  )
+}
+
+export default Add
